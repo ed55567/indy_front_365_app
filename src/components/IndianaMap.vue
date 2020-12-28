ee30be58b7d743b7f160b7d62e67c8d595be13dc
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-12-28 18:45:06 -0500

    added package install

---
+++ src/components/IndianaMap.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id='map'></div>  
+</template>
+
+
+<style>
+#map {
+  width: 400px;
+  height: 300px;
+}
+</style>
+
+<script>
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZWRhbmllbHM1NTUiLCJhIjoiY2tqMHExdnc1MGpoZTJycGhtcWxjemR6cyJ9.HFI61Th4IQguxzjt7kmVYw";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
+</script>
+
--- src/views/ServicesIndex.vue
+++ src/views/ServicesIndex.vue
@@ -1,24 +1,48 @@
 <template>
    <div class="services-index">
-     <h1>All services</h1>
-     <div v-for="service in services">
-       <h2>{{ service.name }}</h2>
-       <img v-bind:src="service.url" v-bind:alt="service.name" />
-       <p>Width: {{ service.width }}</p>
-       <p>Height: {{ service.height }}</p>
+     <h1>All Services</h1>
+     <div id='map'></div> 
+     <div class="dropdown">
+    <input class="dropdown-input" type="text" placeholder="Service Type" />
+    <div class="dropdown-list"></div>
+  </div>
+     <input class="form-control" type="text" v-model="cityTerm" placeholder="Search City" />
+     <div v-for="service in filterBy(services, cityTerm, 'city')">
+     <!-- <div v-for="service in services"> -->
+       <hr>
+        <h2>{{ service.name }}</h2>
+        <!-- <img v-bind:src="service.url" v-bind:alt="service.name" /> -->
+        <p>Service Type: {{ service.service_type }}</p>
+        <p>Address: {{ service.address }}</p>
+        <p>City: {{ service.city }}</p>
+        <router-link to="/photos">Contact Info</router-link>
      </div>
    </div>
- </template>
+</template>
 
 <script>
+import Vue from "vue";
+import Vue2Filters from "vue2-filters";
 import axios from "axios";
 
+Vue.use(Vue2Filters);
+
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function () {
     return {
       services: [],
+      cityTerm: "",
+      // serviceTerm "",
     };
   },
+  //   mapboxgl.accessToken ="pk.eyJ1IjoiZWRhbmllbHM1NTUiLCJhIjoiY2tqMHExdnc1MGpoZTJycGhtcWxjemR6cyJ9.HFI61Th4IQguxzjt7kmVYw";
+  //  var map = new mapboxgl.Map({
+  //   container: "map",
+  //   style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+  //   center: [-74.5, 40], // starting position [lng, lat]
+  //   zoom: 9, // starting zoom
+  // }),
   created: function () {
     axios.get("/api/services").then((response) => {
       console.log("services index", response);
